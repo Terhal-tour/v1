ff5a75ea05412071253d586423684a3ed2746dee
Mariam Elbaz <maruoma.we.bas@gmail.com>
2025-07-16 03:35:46 +0300

    add dashboard

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,24 +1,22 @@
+
+
 // src/layouts/AdminLayout.jsx
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-
+import AdminNavbar from '../components/admin/AdminNavbar';
+import AdminSidebar from '../components/admin/AdminSidebar';
 
 export default function AdminLayout() {
   return (
-    <div className="flex min-h-screen">
-      {/* Sidebar */}
-      
-
-      {/* Content area */}
+    <div className="flex min-h-screen bg-gray-100">
+      <AdminSidebar />
       <div className="flex flex-col flex-1">
-        {/* Top navbar */}
-        
-
-        {/* Main content */}
-        <main className="p-6">
+        <AdminNavbar />
+        <main className="p-6 transition-all duration-300 ease-in-out">
           <Outlet />
         </main>
       </div>
     </div>
   );
 }
+
